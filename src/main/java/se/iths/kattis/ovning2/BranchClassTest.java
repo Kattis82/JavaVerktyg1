289551e3be122efcc86655a0b27c185a268a6608
Kattis82 <kattiscalmvik@outlook.com>
2026-01-20 14:33:20 +0100

    lagt till ny klass, uppdaterat klassen

--- src/main/java/se/iths/kattis/ovning2/BranchClassTest.java
+++ src/main/java/se/iths/kattis/ovning2/BranchClassTest.java
@@ -3,5 +3,7 @@ package se.iths.kattis.ovning2;
 public class BranchClassTest {
     public static void main(String[] args) {
         System.out.println("Testar....");
+        System.out.println("Konflikt");
+
     }
 }
