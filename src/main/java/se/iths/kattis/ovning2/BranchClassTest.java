41c7f8618d15c9ca4236b2fe765a93b56472b76c
J0ssan-iths <Josefine.Berglund@iths.se>
2026-01-20 14:33:01 +0100

    lagt till en text rad

--- src/main/java/se/iths/kattis/ovning2/BranchClassTest.java
+++ src/main/java/se/iths/kattis/ovning2/BranchClassTest.java
@@ -3,5 +3,6 @@ package se.iths.kattis.ovning2;
 public class BranchClassTest {
     public static void main(String[] args) {
         System.out.println("Testar....");
+        System.out.println("Konflikt");
     }
 }
